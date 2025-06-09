ac216b33b8be960592a0db5cde6d2194c71c79db
gstkrsnaprnta <gstkrisnapranata@gmail.com>
2025-06-09 12:29:08 +0800

    create migrations user and admin dinas

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -7,6 +7,13 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 class Admin extends Authenticatable
 {
     protected $guard = 'admin';
-    protected $fillable = ['name', 'email', 'password'];
+    protected $table = 'admin_dinas'; // Sesuaikan dengan tabel AdminDinas
+    protected $primaryKey = 'nip';
+    protected $fillable = ['nip', 'id_user', 'name', 'email', 'password'];
     protected $hidden = ['password', 'remember_token'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'id_user');
+    }
 }
