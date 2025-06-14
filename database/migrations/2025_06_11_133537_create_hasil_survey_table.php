ca774fcee3b9233ca607fc0a06911670237ab1b1
RyianDzuhri <muhammadryiandzuhri@gmail.com>
2025-06-15 00:02:28 +0800

    push tsble

--- database/migrations/2025_06_11_133537_create_hasil_survey_table.php
+++ database/migrations/2025_06_11_133537_create_hasil_survey_table.php
@@ -10,17 +10,17 @@ return new class extends Migration
     {
         Schema::create('hasil_survey', function (Blueprint $table) {
              $table->id();
-            $table->foreignId('pengajuan_id')->unique()->constrained('pengajuan')->onDelete('cascade');
-            $table->foreignId('petugas_nip')->constrained('petugas', 'nip');
-            $table->date('tgl_survey');
-            $table->enum('status_kepemilikan', ['Milik Sendiri', 'Sewa', 'Menumpang', 'Tidak Jelas']);
-            $table->enum('verifikasi_ekonomi', ['Sesuai', 'Tidak Sesuai']);
-            $table->text('detail_verifikasi_dokumen')->nullable();
-            $table->text('catatan_survei')->nullable();
-            $table->text('bukti_survei')->nullable();
-            $table->enum('status_rekomendasi', ['Layak', 'Tidak Layak']);
-            $table->string('alasan_penolakan')->nullable();
-            $table->timestamps();
+                $table->foreignId('pengajuan_id')->unique()->constrained('pengajuan')->onDelete('cascade');
+                $table->bigInteger('petugas_nip');
+                $table->date('tgl_survey');
+                $table->enum('status_kepemilikan', ['Milik Sendiri', 'Sewa', 'Menumpang', 'Tidak Jelas']);
+                $table->enum('verifikasi_ekonomi', ['Sesuai', 'Tidak Sesuai']);
+                $table->text('detail_verifikasi_dokumen')->nullable();
+                $table->text('catatan_survei')->nullable();
+                $table->text('bukti_survei')->nullable();
+                $table->enum('status_rekomendasi', ['Layak', 'Tidak Layak']);
+                $table->string('alasan_penolakan')->nullable();
+                $table->timestamps();
 
             $table->foreign('petugas_nip')->references('nip')->on('petugas')->onDelete('cascade');
         });
